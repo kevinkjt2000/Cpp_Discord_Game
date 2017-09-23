c0fc96ccf2ff169c1b924b0d2104a87fc35caae0
Alpgh367 <Alpgh367@gmail.com>
2017-09-23 12:45:01 +1000

    Create player.cpp

---
+++ src/player.cpp
@@ -0,0 +1,10 @@
+#include "player.h"
+
+std::string player::getName()
+{
+  return name;
+} // needs a new line here
+void player::setName(const std::string& newName)
+{
+  name = newName;
+} // needs rest of implementations below
