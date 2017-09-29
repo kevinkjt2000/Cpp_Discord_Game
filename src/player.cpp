a825aee35dfe72cb0858e5abafc14b2057d101e9
Fede Devi <federico.devigili@tol.info>
2017-09-29 16:27:08 +0200

    Added setters/getters to player

--- src/player.cpp
+++ src/player.cpp
@@ -2,14 +2,25 @@
 
 player::player(const std::string &playername)
 {
-    name = playername;
+  name = playername;
 }
 
 std::string player::getName()
 {
   return name;
-} // needs a new line here
+}
+
 void player::setName(const std::string& newName)
 {
   name = newName;
-} // needs rest of implementations below
+}
+
+unsigned int player::getHealth()
+{
+  return health;
+}
+
+void player::setHealth(const unsigned int newHealth)
+{
+  health = newHealth;
+}
